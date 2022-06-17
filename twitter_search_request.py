21a08ccb1ed2b8f5ce0f3978191a1e98a58a10a3
florian-frey <83298480+florian-frey@users.noreply.github.com>
2022-06-17 16:42:30 +0200

    adjust api request and training

--- twitter_search_request.py
+++ twitter_search_request.py
@@ -1,14 +1,12 @@
-import json
 import requests
 from datetime import datetime
+import json
 
-# Write the necessary tokens into the credentials.json file. DO NOT PUSH that file to GitHub
-credentials = json.load(open('credentials.json'))
-BEARER_TOKEN = credentials["Bearer Token"]
-
+# Insert the Bearer_Token here. DO NOT commit it to keep it secret!
+bearer_token = ""
 
 # define api request function
-def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
+def search_twitter(bearer_token = bearer_token):
     headers = {"Authorization": f"Bearer {bearer_token}"}
 
     url = f"https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}&{expansion}&max_results=100"
@@ -22,25 +20,17 @@ def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
 
 
 # search term
-query = '''
-    gta online
-    -youtube
-    -("buy gta 5 online modded accounts")
-    -("retweet to win")
-    lang:en
-    -is:retweet
-'''
-
-# 'source' operator is only available in enterprise version of the api unfortunately, so we will remove bot tweets later
+game = "gta online"
+query = f"({game}) lang:en -is:retweet -is:reply -is:quote -(twitch) -(youtube)"
 
 # twitter fields to be returned by api call
 tweet_fields = "tweet.fields=text,lang,created_at,public_metrics,geo,source"
 expansion = "expansions=author_id&user.fields=name,username,location,protected,verified"
 
 # twitter api call
-json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
+json_response = search_twitter()
 
 # save to json file
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-with open(f'api_responses/data_{timestamp}.json', 'w') as f:
+with open(f'api_responses/{timestamp}_{game}.json', 'w') as f:
     json.dump(json_response, f, indent=4, sort_keys=True)
